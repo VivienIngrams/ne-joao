242e57f210a3e674a2a53c2d1a1946538e93b25e
VivienIngrams <vivingrams@gmail.com>
2024-10-10 12:55:22 +0100

    Home page

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default async function HomePage() {
+
+
+  return (
+    <section className="relative h-screen bg-black w-full text-white">
+    Labio
+        {/* <Image
+          className="object-cover"
+          src=""
+          fill
+          sizes="100vw"
+          alt="Home Page Background"
+        /> */}
+      
+    </section>
+  );
+}
+
